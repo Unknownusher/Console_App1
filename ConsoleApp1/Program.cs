888b828f241d6b43ed9c3b23c261833b1e07c46c
UNKNOWNUSHER <siah1031@yahoo.com>
2023-09-03 13:10:21 -0400

    Message

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+class main {
+
+    static void Main(String[] args) {
+
+        int x, y;
+
+        Console.Write("Enter a number: ");
+        x = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter another number: ");
+        y = Convert.ToInt32(Console.ReadLine());
+
+        int higherNumber = Max(x, y);
+        int lowerNumber  = Min(x, y);
+
+        Console.WriteLine($"\n{higherNumber} is the higher number and {lowerNumber} is the lower number.");
+        Console.ReadKey();
+
+    }
+
+    static int Min(int x, int y) => (x < y) ? x : y;
+    static int Max(int x, int y) => (x < y) ? y : x;
+
+}
